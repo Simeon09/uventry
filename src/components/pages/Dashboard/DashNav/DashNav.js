ed3369e49b3f00c2dfafc468f0a3c7ba5c43dc9d
unknown <simeonolawale69@gmail.com>
2022-07-23 00:41:37 +0100

    dash bar donr

--- src/components/pages/Dashboard/DashNav/DashNav.js
+++ src/components/pages/Dashboard/DashNav/DashNav.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import './sass/DashNav.css'
+import { FiSearch } from "react-icons/fi";
+import { IoMailOutline,IoNotificationsOutline } from "react-icons/io5";
+import Avatar from '../../../assests/Images/avatar.svg'
 const DashNav = () => {
   return (
-    <div>
-        <div><input placeholder='search...' type='text' name='search'/></div>
-        <div></div>
+    <div className='dashnav'>
+        <div className='searchbar'> <FiSearch/><input placeholder='search...' type='text' name='search'/></div>
+        <div className='Sidenav'>
+       <span><  IoNotificationsOutline className='icondashnav'/> <p className='count'>2</p></span>
+        <span><IoMailOutline className='icondashnav'/><p className='count'>2</p></span>
+        <span className='profileinfo'> <h5>Drey Grafield </h5> <p>View Profile</p></span>
+        <span><img src={Avatar} alt='avatar'/> </span>
+
+        </div>
     </div>
   )
 }
