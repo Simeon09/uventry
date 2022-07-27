8e7de21d8d9cad389fa297f5919e3cf70ca816da
unknown <simeonolawale69@gmail.com>
2022-07-27 21:50:44 +0530

    home

--- src/components/pages/Dashboard/Home.js
+++ src/components/pages/Dashboard/Home.js
@@ -2,6 +2,11 @@ import React from 'react'
 import DashNav from './DashNav/DashNav'
 import './home.css'
 import cross from '../../assests/Images/cross.svg'
+import history from '../../assests/Images/History.svg'
+import product from '../../assests/Images/New Product.svg'
+import shipped from '../../assests/Images/Shipped.svg'
+
+
 const Home = () => {
   return (
     <div className='hom'>
@@ -12,9 +17,9 @@ const Home = () => {
           <span><div><img src={cross} alt='cross'/></div> <div>New</div></span>
         </div>
         <div>
-          <span><small><img src={history} alt='history'/></small></span>
-          <span></span>
-          <span></span>
+          <span className='delivery'><small><img src={history} alt='history'/></small> <p>Required Invoice <br /> <span>10</span> </p> </span>
+          <span  className='delivery'> <small><img src={product} alt='history'/></small> <p>Required packing <br /> <span>5</span></p></span>
+          <span  className='delivery'> <small><img src={shipped} alt='history'/></small> <p>Required Delivery <br /> <span>3</span></p></span>
         </div>
        </div>
     </div>
